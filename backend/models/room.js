90ce618de5619edb9a21ab2abe147dbba859ac19
9600bauds <blasaladio3@gmail.com>
2023-08-11 15:30:28 -0300

    Use helpers for deleting __v

--- backend/models/room.js
+++ backend/models/room.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 const rollSchema = require('./rollSchema');
+const { transformIdAndV } = require('../utils/transformIdAndV');
 
 const roomSchema = new mongoose.Schema({
     name: {
@@ -19,9 +20,7 @@ roomSchema.plugin(uniqueValidator);
 
 roomSchema.set('toJSON', {
     transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString();
-        delete returnedObject._id;
-        delete returnedObject.__v;
+        returnedObject = transformIdAndV(returnedObject);
         // Never reveal the passwordhash!
         delete returnedObject.passwordHash;
     },
