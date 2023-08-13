433fd43c9e55c53945f8fb989ca517a67c720112
9600bauds <blasaladio3@gmail.com>
2023-08-13 01:15:41 -0300

    Fly.io integration

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ require('express-async-errors');
 const app = express();
 const cors = require('cors');
 const middleware = require('./utils/middleware');
+const path = require('path');
 const cookieParser = require('cookie-parser');
 
 const roomRouter = require('./controllers/room');
@@ -27,6 +28,7 @@ const limiter = rateLimit({
     max: 100 // limit each IP to 100 requests per windowMs
 });
 
+app.set('trust proxy', 1); //Is this actually necessary? I added it while trying to make fly.io work
 app.use(cors({
     origin: config.FRONTEND_URL,
     credentials: true
@@ -40,7 +42,11 @@ app.use(cookieParser());
 app.use('/api/login', loginRouter);
 app.use('/api/room', roomRouter);
 
-app.use(middleware.unknownEndpoint);
+// Handles any requests that don't match the ones above
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+ });
+ 
 app.use(middleware.errorHandler);
 
 module.exports = app;
